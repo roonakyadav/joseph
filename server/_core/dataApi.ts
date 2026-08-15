18edda35a9f0926e09fc20c78e671aa0b634ef04
Ronak <ronakyadav1609@gmail.com>
2026-08-15 20:14:04 +0530

    refactor: simplify URL normalization in dataApi

--- server/_core/dataApi.ts
+++ server/_core/dataApi.ts
@@ -25,7 +25,7 @@ export async function callDataApi(
   }
 
   // Build the full URL by appending the service path to the base URL
-  const baseUrl = ENV.forgeApiUrl.endsWith("/") ? ENV.forgeApiUrl : `${ENV.forgeApiUrl}/`;
+  const baseUrl = ENV.forgeApiUrl.replace(/\/+$/, "") + "/";
   const fullUrl = new URL("webdevtoken.v1.WebDevService/CallApi", baseUrl).toString();
 
   const response = await fetch(fullUrl, {
